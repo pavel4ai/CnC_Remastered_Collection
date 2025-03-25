184568ab8d2b4e3d1275f2bbcb1a88c24cba485a
pavel4ai <pavel@centml.ai>
2025-03-25 11:48:27 -0400

    todo app test

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  };
+
+  const handleRemoveTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/TodoList.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  };
+
+  const handleRemoveTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Enter new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
